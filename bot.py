0255aa2966f7eda48bcd7457f82f56bfffb82dda
Achraf99Jday <achraf.jd@gmail.com>
2020-02-23 00:39:20 -0500

    test version

---
+++ bot.py
@@ -0,0 +1,75 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.chrome.options import Options
+
+
+
+
+
+class Bot():
+    def __init__(self):
+        options = Options()
+        options.binary_location = "/usr/bin/google-chrome" #chrome binary location specified here
+        options.add_argument("--start-maximized") #open Browser in maximized mode
+        options.add_argument("--no-sandbox") #bypass OS security model	
+        options.add_argument("--disable-dev-shm-usage") #overcome limited resource problems
+        options.add_experimental_option('useAutomationExtension', False)
+        self.driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=options)
+
+    def login(self):
+        self.driver.get('https://tinder.com')
+        sleep(10)
+
+        facebook_login = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/div/main/div/div[2]/div[2]/div/div/span/div[2]/button') #xpath found using inspect element
+        facebook_login.click()
+
+        # switch to login popup
+        base_window = self.driver.window_handles[0]
+        self.driver.switch_to_window(self.driver.window_handles[1])
+
+        email_in = self.driver.find_element_by_xpath('//*[@id="email"]')
+        email_in.send_keys("username")
+
+        pw_in = self.driver.find_element_by_xpath('//*[@id="pass"]')
+        pw_in.send_keys("password")
+
+        login_btn = self.driver.find_element_by_xpath('//*[@id="u_0_0"]')
+        login_btn.click()
+
+        self.driver.switch_to_window(base_window)
+
+        popup_1 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        popup_1.click()
+
+        popup_2 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        popup_2.click()
+
+    def like(self):
+        like_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]')
+        like_btn.click()
+
+    def dislike(self):
+        dislike_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[1]')
+        dislike_btn.click()
+
+    def auto_swipe(self):
+        while True:
+            sleep(0.5)
+            try:
+                self.like()
+            except Exception:
+                try:
+                    self.close_popup()
+                except Exception:
+                    self.close_match()
+
+    def close_popup(self):
+        popup_3 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[2]/button[2]')
+        popup_3.click()
+
+    def close_match(self):
+        match_popup = self.driver.find_element_by_xpath('//*[@id="modal-manager-canvas"]/div/div/div[1]/div/div[3]/a')
+        match_popup.click()
+
+bot = Bot()
+bot.login()
